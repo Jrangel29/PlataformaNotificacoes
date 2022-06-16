b449432d5ed685aa3faef7a935908f491f6fe957
Jrangel29 <jrangel29@ua.pt>
2022-06-16 18:35:17 +0100

    Detalhes de notificação no histórico

---
+++ src/Components/Cards/UserNotificationCards.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import '../../Styles/Cards.css';
+import '../../Styles/Filters.css';
+import {Card} from 'react-bootstrap';
+
+const UserNotificationCards = (props) => {
+
+    return(            
+        <div className='container m-0 p-0'>
+            <div className='row mt-1'>
+                {props.info.length > 0 ?
+                props.info.map((item, index) => {
+                    return(
+                        <span key={index} className='col-3 mb-2'>
+                            <Card>
+                                <Card.Body className='m-0 cartaGrupos'>
+                                    <span>
+                                        <p className='tituloUserCartaSmall mb-1'>
+                                            {item.nomeUser}
+                                        </p>
+                                        <p className='textoSmall mb-0' style={{fontSize: '14px'}}>
+                                            {item.idade} anos
+                                        </p>
+                                    </span>
+                                </Card.Body>
+                                {props.historico === "sim" ?
+                                <Card.Footer className={item.fechou.data[0] === 0 ? 'recusa m-0' : 'aceita m-0'}>
+                                    {item.fechou.data[0] === 0 ? 'Fechou a notificação' : 'Relembrou'}
+                                </Card.Footer>
+                                :
+                                <></>
+                                }
+                            </Card>
+                        </span>
+                    )
+                })
+                :
+                <span className='col-12' >
+                    Este evento não tem utilizadores.
+                </span>
+            }
+            </div>
+        </div>    
+    )
+}
+
+export default UserNotificationCards;
--- src/Pages/HistoryDetails.js
+++ src/Pages/HistoryDetails.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import Navbar from '../Components/Geral/Navbar';
 import Header from '../Components/Geral/Header';
 import '../Styles/User.css';
-import UserDetailCards from '../Components/Cards/UserDetailCards';
+import UserNotificationCards from '../Components/Cards/UserNotificationCards';
 import {Table} from 'react-bootstrap';
 import { getNotificationInfo } from '../Store/Notifications/Actions';
 import { useSelector, useDispatch } from 'react-redux';
@@ -14,12 +14,49 @@ const HistoryDetails = () => {
     const dispatch = useDispatch();
     const {id} = useParams();
 
+    const [estatiticas, setEstatisticas] = useState({
+        fechou: 0,
+        abriu: 0,
+        recebeu: 0,
+        enviou: 0
+    })
+
     const notificationInfo = useSelector(({ notificacoes }) => notificacoes.data)
     const isLoadingNotificationInfo = useSelector(({ notificacoes }) => notificacoes.isLoadingSingle)
 
     useEffect(() => {
         dispatch(getNotificationInfo(id))
     }, [])
+
+    useEffect(() => {
+
+        var fechou = 0;
+        var abriu = 0;
+        var recebeu = 0;
+        var enviou = 0;
+
+        if(!isLoadingNotificationInfo){
+            notificationInfo.map(item => {
+                if(item.enviado.data[0] !== 0){
+                    enviou++;
+                }
+                if(item.recebido.data[0] !== 0){
+                    recebeu++;
+                }
+                if(item.fechou.data[0] !== 0){
+                    abriu++;
+                } else {
+                    fechou++
+                }
+            })
+            setEstatisticas({
+                fechou: fechou,
+                abriu: abriu,
+                recebeu: recebeu,
+                enviou: enviou
+            })
+        }
+    }, [isLoadingNotificationInfo])
 
     if(isLoadingNotificationInfo){
         return(
@@ -29,7 +66,7 @@ const HistoryDetails = () => {
 
     return(
         <div>
-            {/*console.log(notificationInfo)*/}
+            {console.log(notificationInfo)}
             <div className='mainBodyForm p-0 container'>
             <Navbar/>
                 <Header nome="Histórico" detalhe="sim" apagaMuda="nao"/>
@@ -45,30 +82,52 @@ const HistoryDetails = () => {
                     <p className='textoSeccaoPagina mb-3'>{notificationInfo[0].descricao ? notificationInfo[0].descricao : 'Esta notificação não tem descrição de rodapé.'}</p>
                 </div>
                 <div className='prevSeccao ms-0'>
-                    <h1 className='tituloSeccaoPaginaNotifs'>Interações</h1>
+                    <h1 className='tituloSeccaoPaginaNotifs'>Estatísticas</h1>
                 </div>
-                <div style={{padding: '0 40px'}} className="mx-2">
-                    <p className='subtituloSeccaoPagina mt-2'>Contagem</p>
-                    <Table striped bordered hover className='tabelaHistorico'>
-                        <thead>
-                            <tr>
-                                <th style={{fontSize: '16px'}}>Interação</th>
-                                <th style={{fontSize: '16px'}}>Contagem</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td style={{fontSize: '14px'}}>Fechou</td>
-                                <td style={{fontSize: '14px'}}>0</td>
-                            </tr>
-                            <tr>
-                                <td style={{fontSize: '14px'}}>Relembrar daqui a 15 minutos</td>
-                                <td style={{fontSize: '14px'}}>0</td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                    <p className='subtituloSeccaoPagina'>Interações detalhadas</p>
-                    {/*<UserDetailCards historico="sim"/>*/}
+                <div style={{padding: '0 40px'}} className="mx-2 row">
+                    <p className='subtituloSeccaoPagina mt-2 px-0'>Contagem</p>
+                    <span className='col-6 m-0 p-0'>
+                        <Table striped bordered hover className='tabelaHistorico'>
+                            <thead>
+                                <tr>
+                                    <th style={{fontSize: '16px'}}>Envio/Receção da notificação</th>
+                                    <th style={{fontSize: '16px'}}>Contagem</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td style={{fontSize: '14px'}}>A notificação foi enviada.</td>
+                                    <td style={{fontSize: '14px'}}>{estatiticas.enviou}</td>
+                                </tr>
+                                <tr>
+                                    <td style={{fontSize: '14px'}}>A notificação foi recebida.</td>
+                                    <td style={{fontSize: '14px'}}>{estatiticas.recebeu}</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </span>
+                    <span className='col-6 m-0 p-0'>
+                        <Table striped bordered hover className='tabelaHistorico'>
+                            <thead>
+                                <tr>
+                                    <th style={{fontSize: '16px'}}>Interação</th>
+                                    <th style={{fontSize: '16px'}}>Contagem</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td style={{fontSize: '14px'}}>O utilizador fechou a notificação.</td>
+                                    <td style={{fontSize: '14px'}}>{estatiticas.fechou}</td>
+                                </tr>
+                                <tr>
+                                    <td style={{fontSize: '14px'}}>O especatador escolheu "Relembrar daqui a 15 minutos".</td>
+                                    <td style={{fontSize: '14px'}}>{estatiticas.abriu}</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </span>
+                    <p className='subtituloSeccaoPagina px-0'>Interações detalhadas</p>
+                    {<UserNotificationCards historico="sim" info={notificationInfo}/>}
                 </div>
             </div>
         </div>
